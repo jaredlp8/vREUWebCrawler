ba0bfc31fbd8310fcf2ae32f287fff710c1a39a3
jaredlp8 <User@DESKTOP-GG5K8RK>
2020-08-21 18:53:38 -0400

    Added code for tutorial

--- crawler.py
+++ crawler.py
@@ -1,8 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
+import lxml
 
-source = requests.get('http://coreyms.com').text
+source = requests.get('http://deadline.com').text
 
-soup = BeautifulSoup(source, 'html.parser')
+soup = BeautifulSoup(source, 'lxml')
 
+article = soup.find('article')
+
+# headline = article.h3.a.text
+# print(headline)
 print(soup.prettify())
+
+# for article in soup.find_all('article'):
+#     headline = article.h3.a.text
+#     print(headline)
+
