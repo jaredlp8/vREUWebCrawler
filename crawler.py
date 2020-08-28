621cb5efbc6f0606623f8f5d75c53ec66c629356
jaredlp8 <User@DESKTOP-GG5K8RK>
2020-08-28 19:24:08 -0400

    Added number of JS files for each site on csv

--- crawler.py
+++ crawler.py
@@ -18,15 +18,19 @@ for url_link in urls:
     x = requests.head(url_link)
     soup_source = soup.prettify()
     js_links = [i.get('src') for i in soup.find_all('script') if i.get('src')]
+    y = requests.get(url_link)
     # prints for testing purposes
     # print("Website:", url_link)
     # print("HTTP header:", x.headers)
     # print("Crawled HTML: ", soup_source)
     # print("JavaScript files: ", js_links)
+    # print(y.headers)
+    # print(len(js_links))
 
     data.append({'Website': url_link,
                  'Header': x.headers,
-                 'JavaScript files': js_links})
+                 'JavaScript files': js_links,
+                 '# of JS files': len(js_links)})
 
     temp_df = pd.DataFrame(data)  # temporary storage
     results_df = results_df.append(temp_df).reset_index(drop=True)  # stores all the data
