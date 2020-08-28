320b3703caa2ed26fa239b5749670b269d5adb00
jaredlp8 <User@DESKTOP-GG5K8RK>
2020-08-27 20:01:41 -0400

    Can crawl multiple websites and gets the header

--- crawler.py
+++ crawler.py
@@ -2,13 +2,23 @@ import bs4 as bs
 import urllib.request
 from requests_html import HTMLSession
 import lxml
+import csv
+import pandas as pd
+url = ['https://google.com', 'https://amazon.com']
 
-session = HTMLSession()
-resp = session.get('https://google.com')
-resp.html.render()
-soup = bs.BeautifulSoup(resp.html.html,'lxml')
-head_tags = soup.find('head')
-print(head_tags.prettify())
-
-
-
+# gets header
+for index in url:
+    session = HTMLSession()
+    resp = session.get(index)
+    resp.html.render()
+    soup = bs.BeautifulSoup(resp.html.html,'lxml')
+    head_tags = soup.find('head')
+    print(index)
+    print(head_tags.prettify())
+    # test_df = pd.DataFrame({'Website': index,
+    #                         'Header': head_tags
+    # })
+    # cols = ['Website',
+    #         'Header']
+    # test_df.to_csv('data/websiteHeaders.csv',
+    #                encoding='utf-8', index=False, columns=cols)
