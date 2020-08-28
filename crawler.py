a4a241238d30006dcad0f3fa7bfafc164e0f1d09
jaredlp8 <User@DESKTOP-GG5K8RK>
2020-08-27 22:26:48 -0400

    Gets HTTP head. .csv file generation in progress

--- crawler.py
+++ crawler.py
@@ -1,6 +1,8 @@
 import bs4 as bs
 import urllib.request
+import requests
 from requests_html import HTMLSession
+from urllib.parse import urljoin
 import lxml
 import csv
 import pandas as pd
@@ -12,9 +14,10 @@ for index in url:
     resp = session.get(index)
     resp.html.render()
     soup = bs.BeautifulSoup(resp.html.html,'lxml')
-    head_tags = soup.find('head')
-    print(index)
-    print(head_tags.prettify())
+    x = requests.head(index)
+    print("Website:", index)
+    print("HTTP header:",x.headers)
+    print("Crawled HTML: ",soup.prettify())
     # test_df = pd.DataFrame({'Website': index,
     #                         'Header': head_tags
     # })
@@ -22,3 +25,17 @@ for index in url:
     #         'Header']
     # test_df.to_csv('data/websiteHeaders.csv',
     #                encoding='utf-8', index=False, columns=cols)
+
+# # get JavaScript files (work in progress)
+# script_files = []
+#
+# for script in soup.find_all("script"):
+#     if script.attrs.get("src"):
+#         script_url = urljoin(url, script.attrs.get("src"))
+#         script_files.append(script_url)
+#
+# header_files = []
+# # gets header
+# for script in soup.find_all("head"):
+#     script_url = urljoin(url, script.attrs.get("head"))
+#     header_files.append(script_url)
