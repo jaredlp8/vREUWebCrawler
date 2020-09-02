074994ffeabe435991cc6440ab62431cd00a6a0c
jaredlp8 <User@DESKTOP-GG5K8RK>
2020-09-01 20:46:16 -0400

    Added csv creation to headless, started data analysis

--- crawler.py
+++ crawler.py
@@ -5,7 +5,8 @@ from requests_html import HTMLSession
 from urllib.parse import urljoin
 import csv
 import pandas as pd
-urls = ['https://google.com', 'https://amazon.com']
+import time
+urls = ['https://cnn.com']
 data = []
 
 results_df = pd.DataFrame()
@@ -15,30 +16,28 @@ for url_link in urls:
     resp = session.get(url_link)
     resp.html.render()
     soup = bs.BeautifulSoup(resp.html.html, 'lxml')
-    x = requests.head(url_link)
+    get_head = requests.head(url_link)
     soup_source = soup.prettify()
     js_links = [i.get('src') for i in soup.find_all('script') if i.get('src')]
-    y = requests.get(url_link)
+    site_header = requests.get(url_link)
     # prints for testing purposes
-    # print("Website:", url_link)
-    # print("HTTP header:", x.headers)
-    # print("Crawled HTML: ", soup_source)
-    # print("JavaScript files: ", js_links)
-    # print(y.headers)
-    # print(len(js_links))
+    print("Website:", url_link)
+    print("HTTP header:", get_head.headers)
+    print("Crawled HTML: ", soup_source)
+    print("JavaScript files: ", js_links)
+    print(site_header.headers)
+    print(len(js_links))
 
     data.append({'Website': url_link,
-                 'Header': x.headers,
+                 'Header': site_header.headers,
                  'JavaScript files': js_links,
                  '# of JS files': len(js_links)})
-
     temp_df = pd.DataFrame(data)  # temporary storage
     results_df = results_df.append(temp_df).reset_index(drop=True)  # stores all the data
 
 # creates the data csv
-results_df.to_csv('data.csv', index=False)
-
-testing = pd.read_csv('data.csv')
+results_df.to_csv('Sites1-5data.csv', index=False)
+testing = pd.read_csv('Sites1-5data.csv')
 heading = testing.head(100)
 description = testing.describe()
 print(heading)
--- headless.py
+++ headless.py
@@ -1,11 +1,34 @@
+import pandas as pd
+import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 # headless browser to test javascript code
 driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get('https://google.com')  # add desired link to test
-
+site = 'https://www.office.com/?omkt=es-CL&auth=2'
+driver.get(site)  # add desired link to test
+y = requests.get(site)
+data = []
+results_df = pd.DataFrame()
 soup = BeautifulSoup(driver.page_source, "lxml")
 javascript_crawl = [i.get('src') for i in soup.find_all('script') if i.get('src')]  # gets javascript
 
 __import__('pprint').pprint(javascript_crawl)
+print(javascript_crawl)
+print(len(javascript_crawl))
+
+data.append({'Website': site,
+             'Header': y.headers,
+             'JavaScript files': javascript_crawl,
+             '# of JS files': len(javascript_crawl)})
+
+temp_df = pd.DataFrame(data)  # temporary storage
+results_df = results_df.append(temp_df).reset_index(drop=True)  # stores all the data
+
+# creates the data csv
+results_df.to_csv('headlessdata.csv', index=False)
+
+testing = pd.read_csv('headlessdata.csv')
+heading = testing.head()
+description = testing.describe()
+print(heading)
